6c5beac1c1e5a1849869c6514ea83b8967c1d550
Khin Su Su Han <virgokhinsu@gmail.com>
2020-09-21 08:20:05 +0630

    Data Detail

---
+++ resources/views/myhome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Home Page</title>
+</head>
+<body>
+	<h2>This is my home page!</h2>
+	<a href="{{ route('mainpage',7) }}"> Go to Welcome Page</a>
+</body>
+</html>
